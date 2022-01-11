567f5098c51476c5829733579906364f895439bc
SmartyPie <95571485+KosmoD@users.noreply.github.com>
2022-01-11 16:29:38 +0530

    Update function.js

--- js/function.js
+++ js/function.js
@@ -1,4 +1,3 @@
-const delay = ms => new Promise(res => setTimeout(res, ms));
 var isValid = false;
 var cells = [];
 var player = 1;
@@ -87,18 +86,16 @@ function loop(){
  
     var winner = whoWon(cells);
     if(winner){
-   if(winner === "computer"){
-    await delay(10000);
+   if(winner === "computer"){   
    Swal.fire("Sad you lost.");
    }else{
-    await delay(10000);
- Swal.fire("Congratulation!! You Won");
+    Swal.fire("Congratulation!! You Won");
       }
    result = winner;
     }
     else if(isTableFull(cells)){
      result = "tie";
-     await delay(10000);
+    
       Swal.fire("It was a " + result);
     }
     if(result !== ""){
