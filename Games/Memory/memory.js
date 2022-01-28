d564cb06026e88eb2d25170192decd7941bdd527
SmartyPie <95571485+KosmoD@users.noreply.github.com>
2022-01-28 20:22:41 +0530

    Update memory.js

--- Games/Memory/memory.js
+++ Games/Memory/memory.js
@@ -1,215 +1,234 @@
-; {
-  let
-    selected1,
-    selected2;
+// cards array holds all cards
+let card = document.getElementsByClassName("card");
+let cards = [...card];
 
-  // http://unicode.org/emoji/charts/full-emoji-list.html
-  const iconAnimals = [
-    '🐵',
-    '🐶',
-    '🐺',
-    '🐱',
-    '🐯',
-    '🐴',
-    '🐮',
-    '🐷',
-    '🐗',
-    '🐭',
-    '🐹',
-    '🐰',
-    '🐻',
-    '🐨',
-    '🐼',
-    '🐔',
-    '🐤',
-    '🐦',
-    '🐧',
-    '🐸',
-    //'🐢	',
-    //'🐍',
-    //'🐳',
-    //'🐬',
-    //'🐠',
-    //'🐙',
-  ];
-  const iconFruits = [
-    '🍇',
-    '🍈',
-    '🍉',
-    '🍊',
-    '🍋',
-    '🍌',
-    '🍍',
-    '🍎',
-    '🍏',
-    '🍑',
-    '🍒',
-    '🍓',
-  ];
+// deck of all cards in game
+const deck = document.getElementById("card-deck");
 
-  let icons = iconAnimals;
+// declaring move variable
+let moves = 0;
+let counter = document.querySelector(".moves");
 
-  const app = angular
-    // Using ngTouch to remove the 300ms click delay on mobile devices
-    .module('app', ['ngRoute', 'ngAnimate', 'ngTouch'])
-    .controller('AppController', AppController);
+// declare variables for star icons
+const stars = document.querySelectorAll(".fa-star");
 
-  AppController.$inject = ['$scope', '$timeout', '$animate'];
-  function AppController($scope, $timeout, $animate) {
+// declaring variable of matchedCards
+let matchedCard = document.getElementsByClassName("match");
 
-    // This is a prototype, everything is in this controller.
-    // For cleaner code you probably would refactor stuff out from here. 
-    // I also use DOM manipulation for faster dev (prototyping)
+// stars list
+let starsList = document.querySelectorAll(".stars li");
 
-    const vm = this;
+// close icon in modal
+let closeicon = document.querySelector(".close");
 
-    $timeout(() => {
-      // Angular DOM has finished rendering
-      $scope.appReady = true;
+// declare modal
+let modal = document.getElementById("popup1");
+
+// array for opened cards
+var openedCards = [];
+
+// @description shuffles cards
+// @param {array}
+// @returns shuffledarray
+function shuffle(array) {
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+// @description shuffles cards when page is refreshed / loads
+document.body.onload = startGame();
+
+// @description function to start a new play
+function startGame() {
+  // empty the openCards array
+  openedCards = [];
+
+  // shuffle deck
+  cards = shuffle(cards);
+  // remove all exisiting classes from each card
+  for (var i = 0; i < cards.length; i++) {
+    deck.innerHTML = "";
+    [].forEach.call(cards, function (item) {
+      deck.appendChild(item);
     });
-
-    function showAllCards() {
-      $scope.list.forEach((card) => {
-        $("#card-" + card.id).flip(true);
-      });
-    }
-
-    function startNewGame() {
-
-      shuffleArray(icons); // randomize displayed icons        
-
-      $animate.enabled(false); // disable remove animation
-
-      $scope.list = [];
-
-      //$animate.enabled(true); // re-enable animation
-
-      let list = [],
-        cardCount = 16;
-      for (var i = 1; i <= cardCount; i++) {
-        let type = (i % (cardCount / 2)) + 1;
-        list.push({
-          id: i,
-          type: type,
-          icon: icons[type - 1],
-          theme: 'theme' + type,
-          isKnown: false
-        });
-      }
-
-      shuffleArray(list); // randomize card on the board
-
-      // Update state
-      selected1 = selected2 = null;
-      $scope.clickCount = 0;
-
-      $scope.cardsLeft = list.length;
-      $scope.gameCompleted = false;
-      $scope.titleAction = () => {};
-      $scope.changeIcons = () => {
-        icons = icons === iconFruits ? iconAnimals : iconFruits;
-      };
-
-      $timeout(() => {
-        // Angular DOM has finished rendering
-
-        $animate.enabled(true); // re-enable animation
-
-        $scope.list = list;
-
-        $timeout(() => {
-          // Angular DOM has finished rendering
-
-          // Apply flip to the cards in the DOM
-          let $card = $(".app__cards-container__card");
-          $card.flip({
-            axis: 'y',
-            trigger: 'manual',
-            reverse: true
-          }, () => {
-            //callback
-          });
-
-          //showAllCards(); // debug
-        });
-      });
-    }
-
-    $scope.clickCount = 0;
-    $scope.cardsLeft = 0;
-    $scope.gameCompleted = false;
-    $scope.restart = () => {
-      startNewGame();
-    };
-
-    $scope.click = function(card, index) {
-
-      if (card.flipped) return; // dont allow to flip already flipped cards
-
-      $scope.clickCount++;
-
-      // Update state
-      card.flipped = true;
-      selected2 = selected1;
-      selected1 = card;
-
-      // Update UI
-      $("#card-" + card.id).flip(true); // use flip api
-
-      if (selected1 && selected2 && selected1.type === selected2.type) {
-        // We found a match
-
-        $scope.list.splice($scope.list.indexOf(selected1), 1);
-        $scope.list.splice($scope.list.indexOf(selected2), 1);
-        selected1 = selected2 = null;
-
-        $scope.cardsLeft = $scope.list.length;
-
-        if ($scope.list.length === 0) {
-          // We have finished the game
-          $scope.gameCompleted = true;
-        }
-
-        return;
-      }
-
-      if (selected2) {
-
-        // Flip back the 2nd shown card
-
-        let id = selected2.id;
-        ((id) => {
-          $timeout(() => {
-            $scope.list.forEach((card) => {
-              if (card.id === id) {
-                // Update state
-                card.flipped = false;
-                card.isKnown = true;
-              }
-            });
-
-            $("#card-" + id).flip(false); // use flip api
-
-          }, 800);
-        })(id);
-      }
-    }
-
-    startNewGame();
+    cards[i].classList.remove("show", "open", "match", "disabled");
   }
-
-  /**
-   * Randomize array element order in-place.
-   * Durstenfeld shuffle algorithm.   
-   */
-  function shuffleArray(array) {
-    for (var i = array.length - 1; i > 0; i--) {
-      var j = Math.floor(Math.random() * (i + 1));
-      var temp = array[i];
-      array[i] = array[j];
-      array[j] = temp;
-    }
-    return array;
+  // reset moves
+  moves = 0;
+  counter.innerHTML = moves;
+  // reset rating
+  for (var i = 0; i < stars.length; i++) {
+    stars[i].style.color = "#FFD700";
+    stars[i].style.visibility = "visible";
   }
+  //reset timer
+  second = 0;
+  minute = 0;
+  hour = 0;
+  var timer = document.querySelector(".timer");
+  timer.innerHTML = "0 mins 0 secs";
+  clearInterval(interval);
+}
 
+// @description toggles open and show class to display cards
+var displayCard = function () {
+  this.classList.toggle("open");
+  this.classList.toggle("show");
+  this.classList.toggle("disabled");
 };
+
+// @description add opened cards to OpenedCards list and check if cards are match or not
+function cardOpen() {
+  openedCards.push(this);
+  var len = openedCards.length;
+  if (len === 2) {
+    moveCounter();
+    if (openedCards[0].type === openedCards[1].type) {
+      matched();
+    } else {
+      unmatched();
+    }
+  }
+}
+
+// @description when cards match
+function matched() {
+  openedCards[0].classList.add("match", "disabled");
+  openedCards[1].classList.add("match", "disabled");
+  openedCards[0].classList.remove("show", "open", "no-event");
+  openedCards[1].classList.remove("show", "open", "no-event");
+  openedCards = [];
+}
+
+// description when cards don't match
+function unmatched() {
+  openedCards[0].classList.add("unmatched");
+  openedCards[1].classList.add("unmatched");
+  disable();
+  setTimeout(function () {
+    openedCards[0].classList.remove("show", "open", "no-event", "unmatched");
+    openedCards[1].classList.remove("show", "open", "no-event", "unmatched");
+    enable();
+    openedCards = [];
+  }, 1100);
+}
+
+// @description disable cards temporarily
+function disable() {
+  Array.prototype.filter.call(cards, function (card) {
+    card.classList.add("disabled");
+  });
+}
+
+// @description enable cards and disable matched cards
+function enable() {
+  Array.prototype.filter.call(cards, function (card) {
+    card.classList.remove("disabled");
+    for (var i = 0; i < matchedCard.length; i++) {
+      matchedCard[i].classList.add("disabled");
+    }
+  });
+}
+
+// @description count player's moves
+function moveCounter() {
+  moves++;
+  counter.innerHTML = moves;
+  //start timer on first click
+  if (moves == 1) {
+    second = 0;
+    minute = 0;
+    hour = 0;
+    startTimer();
+  }
+  // setting rates based on moves
+  if (moves > 8 && moves < 12) {
+    for (i = 0; i < 3; i++) {
+      if (i > 1) {
+        stars[i].style.visibility = "collapse";
+      }
+    }
+  } else if (moves > 13) {
+    for (i = 0; i < 3; i++) {
+      if (i > 0) {
+        stars[i].style.visibility = "collapse";
+      }
+    }
+  }
+}
+
+// @description game timer
+var second = 0,
+  minute = 0;
+hour = 0;
+var timer = document.querySelector(".timer");
+var interval;
+function startTimer() {
+  interval = setInterval(function () {
+    timer.innerHTML = minute + "mins " + second + "secs";
+    second++;
+    if (second == 60) {
+      minute++;
+      second = 0;
+    }
+    if (minute == 60) {
+      hour++;
+      minute = 0;
+    }
+  }, 1000);
+}
+
+// @description congratulations when all cards match, show modal and moves, time and rating
+function congratulations() {
+  if (matchedCard.length == 16) {
+    clearInterval(interval);
+    finalTime = timer.innerHTML;
+
+    // show congratulations modal
+    modal.classList.add("show");
+
+    // declare star rating variable
+    var starRating = document.querySelector(".stars").innerHTML;
+
+    //showing move, rating, time on modal
+    document.getElementById("finalMove").innerHTML = moves;
+    document.getElementById("starRating").innerHTML = starRating;
+    document.getElementById("totalTime").innerHTML = finalTime;
+
+    //closeicon on modal
+    closeModal();
+  }
+}
+
+// @description close icon on modal
+function closeModal() {
+  closeicon.addEventListener("click", function (e) {
+    modal.classList.remove("show");
+    startGame();
+  });
+}
+
+// @desciption for user to play Again
+function playAgain() {
+  modal.classList.remove("show");
+  startGame();
+}
+
+// loop to add event listeners to each card
+for (var i = 0; i < cards.length; i++) {
+  card = cards[i];
+  card.addEventListener("click", displayCard);
+  card.addEventListener("click", cardOpen);
+  card.addEventListener("click", congratulations);
+}
