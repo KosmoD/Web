f2e6953bf1802db8c00a6646b38875664dc9a8fb
SmartyPie <95571485+KosmoD@users.noreply.github.com>
2022-01-29 11:24:38 +0530

    Update script.js

--- Games/Memory/script.js
+++ Games/Memory/script.js
@@ -63,7 +63,7 @@ document.addEventListener('DOMContentLoaded', () => {
   function createBoard() {
     for (let i = 0; i < cardArray.length; i++) {
       const card = document.createElement('img')
-      card.setAttribute('src', 'images/blank.png')
+      card.setAttribute('src', 'images/question.png')
       card.setAttribute('data-id', i)
       card.addEventListener('click', flipCard)
       grid.appendChild(card)
